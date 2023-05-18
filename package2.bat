52046cb81079949ba23deebe4b1a221aab72595d
GANGIREDDY VISHNU VARDHAN REDDY <76578959+VVR1102001@users.noreply.github.com>
2023-05-19 00:02:14 +0530

    Add files via upload

---
+++ package2.bat
@@ -0,0 +1,12 @@
+pip install pandas==1.5.2 
+pip install sklearn
+pip install matplotlib==3.6.2
+pip install seaborn==0.12.2
+pip install numpy==1.23.5
+pip install scipy==1.1.0
+pip install scikit-learn==1.2.2
+pip install keras==2.12.0
+pip install tensorflow==2.12.0
+pip install h5py==3.8.0
+
+pip install flask
